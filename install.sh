6dc16e062009bfedc0d2096c6983f79dcf939824
Michael Snowden <michael.snowden@glassdoor.com>
2016-01-24 18:41:37 -0800

    Removed opening the REPL after installation

--- install.sh
+++ install.sh
@@ -9,5 +9,4 @@ echo "java -jar $(pwd)/target/unit_converter.jar \$1" >> unit_converter.sh
 sudo chmod 777 unit_converter.sh
 sudo cp unit_converter.sh /usr/bin/unit_converter
 echo "You can now type 'unit_converter' from any directory to open the REPL"
-echo "Installation complete! We will now open the REPL"
-unit_converter
+echo "Installation complete!"
