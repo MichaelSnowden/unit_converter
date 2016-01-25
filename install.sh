0b2c9a4ccc0a7202094b0bede7a0fc0aff219e70
Michael Snowden <michael.snowden@glassdoor.com>
2016-01-24 18:44:28 -0800

    Made post-install instructions clearer

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ echo "#!/usr/bin/env bash" >> unit_converter.sh
 echo "java -jar $(pwd)/target/unit_converter.jar \$1" >> unit_converter.sh
 sudo chmod 777 unit_converter.sh
 sudo cp unit_converter.sh /usr/bin/unit_converter
-echo "You can now type 'unit_converter' from any directory to open the REPL"
 echo "Installation complete!"
+echo "Open the REPL with this command:"
+echo "    unit_converter"
