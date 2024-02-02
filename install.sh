e8405d3d4c7fae4257216f0262aa247cfad32e54
Michael Snowden <MichaelSnowden@users.noreply.github.com>
2024-02-02 11:26:58 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mvn clean compile assembly:single
 echo "#!/usr/bin/env bash" >> unit_converter.sh
 echo "java -jar $(pwd)/target/unit_converter.jar \$1" >> unit_converter.sh
 sudo chmod 777 unit_converter.sh
-sudo cp unit_converter.sh /usr/bin/unit_converter
+sudo cp unit_converter.sh /usr/local/bin/unit_converter
 echo "Installation complete!"
 echo "Open the REPL with this command:"
 echo "    unit_converter"
